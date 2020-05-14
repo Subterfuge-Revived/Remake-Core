9f24c3fc8d3e2cb6ad3da15de5385df8ce00f53a
Quinn Bast <quinn.bast@hotmail.com>
2020-05-13 19:23:22 -0600

    Updated unit tests

--- SubterfugeCoreTest/GameTick.test.cs
+++ SubterfugeCoreTest/GameTick.test.cs
@@ -92,7 +92,8 @@ namespace SubterfugeCoreTest
             GameTick newTick = GameTick.FromDate(newDate);
 
             Assert.AreEqual(_tick.GetTick() + numberOfTicks, newTick.GetTick());
-            Assert.AreEqual(newDate, newTick.GetDate());
+            Assert.AreEqual(newDate.ToLongTimeString(), newTick.GetDate().ToLongTimeString());
+            Assert.AreEqual(newDate.ToLongDateString(), newTick.GetDate().ToLongDateString());
         }
 
         [TestMethod]
@@ -104,7 +105,8 @@ namespace SubterfugeCoreTest
             GameTick newTick = GameTick.FromTickNumber(numberOfTicks);
 
             Assert.AreEqual(_tick.GetTick() + numberOfTicks, newTick.GetTick());
-            Assert.AreEqual(newDate, newTick.GetDate());
+            Assert.AreEqual(newDate.ToLongTimeString(), newTick.GetDate().ToLongTimeString());
+            Assert.AreEqual(newDate.ToLongDateString(), newTick.GetDate().ToLongDateString());
 
         }
 
