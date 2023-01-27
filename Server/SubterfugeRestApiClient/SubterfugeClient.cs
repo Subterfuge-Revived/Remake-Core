391e4b4ebe1054f589abc9cd069ff20362863922
Quinn Bast <quinn.bast@hotmail.com>
2023-01-27 01:27:56 -0600

    Update the readmes and add some comments to the API

--- Server/SubterfugeRestApiClient/SubterfugeClient.cs
+++ Server/SubterfugeRestApiClient/SubterfugeClient.cs
@@ -20,7 +20,15 @@ namespace SubterfugeRestApiClient
         public HealthClient HealthClient;
         public SocialClient SocialClient;
         public SpecialistClient SpecialistClient;
-
+        
+        /// <summary>
+        /// The client constructor.
+        ///
+        /// By default, this constructor works for local development as well as docker container environments.
+        /// The client will look for the `ASPNETCORE_ENVIRONMENT` environment variable.
+        /// If the environment variable is `Docker`, the client connects to: "server:5295", otherwise it will connect to "localhost:5295".
+        /// If you need to connect to a custom server URL, use the secondary constructor which accepts a base URL.
+        /// </summary>
         public SubterfugeClient()
         {
             string hostname = "localhost";
@@ -35,6 +43,10 @@ namespace SubterfugeRestApiClient
             setupClient($"http://{hostname}:{port}/");
         }
         
+        /// <summary>
+        /// Creates an instance of a subterfuge client using the specified baseURL.
+        /// </summary>
+        /// <param name="baseUrl">The server URL to connect to. Should include the protocol, hostname, and port. ex. http://localhost:5295</param>
         public SubterfugeClient(string baseUrl)
         {
             setupClient(baseUrl);
